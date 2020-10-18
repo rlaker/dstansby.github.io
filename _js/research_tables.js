e7d2e08cacfba93a959538a49de355c1a505f4cb
David Stansby <dstansby@gmail.com>
2020-10-18 12:27:17 +0100

    Add table class

--- _js/research_tables.js
+++ _js/research_tables.js
@@ -1,30 +1,51 @@
 'use strict';
 
 
+class Table extends React.Component {
+
+  render() {
+    return (
+      <table border="1px solid black" style={{borderCollapse: 'collapse'}}>
+      {this.props.header}
+      {this.props.rows}
+      </table>
+    )
+  }
+}
+
+class TableRows extends React.Component {
+  render () {
+    const items = this.props.items.map(
+      (item) => <tr key={item['key']}>{item}</tr>
+    );
+    return (
+      <tbody>
+      {items}
+      </tbody>
+    );
+  }
+}
+
+
 class Talk extends React.Component {
+  constructor(props) {
+    super(props);
+    this.key = props.title;
+  }
+
   render() {
     return (
       <React.Fragment>
        <td>{this.props.date}</td>
-       <td><a href={this.props.file}>{this.props.title}</a></td>
+       <td><a href={"talks/" + this.props.file}>{this.props.title}</a></td>
        <td>{this.props.location}</td>
       </React.Fragment>
     );
   }
 }
 
-const talks = [
-  <Talk
-  date='14/07/2020'
-  file='20200731UKSP.pdf'
-  title='Sensitivity of solar wind mass flux to coronal temperature'
-  location='UK Solar Physics 2020 Discussion Meeting'
-  />
-];
-
-const talkItems = talks.map((talk) => <tr key={talk.props.file}>{talk}</tr>);
-
-const header = (
+// Create variables for talks
+const talksHeader = (
   <thead>
    <tr>
      <th align='left' padding='8px'>Date</th>
@@ -33,20 +54,16 @@ const header = (
    </tr>
    </thead>
 )
-
-class TalkTable extends React.Component {
-
-  render() {
-    return (
-      <table border="1px solid black" style={{borderCollapse: 'collapse'}}>
-      {header}
-       <tbody>
-       {talkItems}
-       </tbody>
-      </table>
-    )
-  }
-}
+const talks = [
+  <Talk
+  date='14/07/2020'
+  file='20200731UKSP.pdf'
+  title='Sensitivity of solar wind mass flux to coronal temperature'
+  location='UK Solar Physics 2020 Discussion Meeting'
+  />
+];
+const talkRows = <TableRows items={talks} keyname='title'/>;
+const talkTable = <Table header={talksHeader} rows={talkRows}/>;
 
 const domContainer = document.querySelector('#talks_table');
-ReactDOM.render(React.createElement(TalkTable), domContainer);
+ReactDOM.render(talkTable, domContainer);
--- research_tables.js
+++ research_tables.js
@@ -8,13 +8,70 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var Talk = function (_React$Component) {
-  _inherits(Talk, _React$Component);
+var Table = function (_React$Component) {
+  _inherits(Table, _React$Component);
 
-  function Talk() {
+  function Table() {
+    _classCallCheck(this, Table);
+
+    return _possibleConstructorReturn(this, (Table.__proto__ || Object.getPrototypeOf(Table)).apply(this, arguments));
+  }
+
+  _createClass(Table, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'table',
+        { border: '1px solid black', style: { borderCollapse: 'collapse' } },
+        this.props.header,
+        this.props.rows
+      );
+    }
+  }]);
+
+  return Table;
+}(React.Component);
+
+var TableRows = function (_React$Component2) {
+  _inherits(TableRows, _React$Component2);
+
+  function TableRows() {
+    _classCallCheck(this, TableRows);
+
+    return _possibleConstructorReturn(this, (TableRows.__proto__ || Object.getPrototypeOf(TableRows)).apply(this, arguments));
+  }
+
+  _createClass(TableRows, [{
+    key: 'render',
+    value: function render() {
+      var items = this.props.items.map(function (item) {
+        return React.createElement(
+          'tr',
+          { key: item['key'] },
+          item
+        );
+      });
+      return React.createElement(
+        'tbody',
+        null,
+        items
+      );
+    }
+  }]);
+
+  return TableRows;
+}(React.Component);
+
+var Talk = function (_React$Component3) {
+  _inherits(Talk, _React$Component3);
+
+  function Talk(props) {
     _classCallCheck(this, Talk);
 
-    return _possibleConstructorReturn(this, (Talk.__proto__ || Object.getPrototypeOf(Talk)).apply(this, arguments));
+    var _this3 = _possibleConstructorReturn(this, (Talk.__proto__ || Object.getPrototypeOf(Talk)).call(this, props));
+
+    _this3.key = props.title;
+    return _this3;
   }
 
   _createClass(Talk, [{
@@ -33,7 +90,7 @@ var Talk = function (_React$Component) {
           null,
           React.createElement(
             'a',
-            { href: this.props.file },
+            { href: "talks/" + this.props.file },
             this.props.title
           )
         ),
@@ -49,22 +106,10 @@ var Talk = function (_React$Component) {
   return Talk;
 }(React.Component);
 
-var talks = [React.createElement(Talk, {
-  date: '14/07/2020',
-  file: '20200731UKSP.pdf',
-  title: 'Sensitivity of solar wind mass flux to coronal temperature',
-  location: 'UK Solar Physics 2020 Discussion Meeting'
-})];
+// Create variables for talks
 
-var talkItems = talks.map(function (talk) {
-  return React.createElement(
-    'tr',
-    { key: talk.props.file },
-    talk
-  );
-});
 
-var header = React.createElement(
+var talksHeader = React.createElement(
   'thead',
   null,
   React.createElement(
@@ -87,34 +132,14 @@ var header = React.createElement(
     )
   )
 );
-
-var TalkTable = function (_React$Component2) {
-  _inherits(TalkTable, _React$Component2);
-
-  function TalkTable() {
-    _classCallCheck(this, TalkTable);
-
-    return _possibleConstructorReturn(this, (TalkTable.__proto__ || Object.getPrototypeOf(TalkTable)).apply(this, arguments));
-  }
-
-  _createClass(TalkTable, [{
-    key: 'render',
-    value: function render() {
-      return React.createElement(
-        'table',
-        { border: '1px solid black', style: { borderCollapse: 'collapse' } },
-        header,
-        React.createElement(
-          'tbody',
-          null,
-          talkItems
-        )
-      );
-    }
-  }]);
-
-  return TalkTable;
-}(React.Component);
+var talks = [React.createElement(Talk, {
+  date: '14/07/2020',
+  file: '20200731UKSP.pdf',
+  title: 'Sensitivity of solar wind mass flux to coronal temperature',
+  location: 'UK Solar Physics 2020 Discussion Meeting'
+})];
+var talkRows = React.createElement(TableRows, { items: talks, keyname: 'title' });
+var talkTable = React.createElement(Table, { header: talksHeader, rows: talkRows });
 
 var domContainer = document.querySelector('#talks_table');
-ReactDOM.render(React.createElement(TalkTable), domContainer);
+ReactDOM.render(talkTable, domContainer);
